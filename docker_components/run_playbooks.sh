7516454437e2afc1ece5e737f39470cb0b1c8129
Behic <behic.erdem@argela.com>
2025-01-23 17:27:09 +0300

    ansible connection errors fixed

--- docker_components/run_playbooks.sh
+++ docker_components/run_playbooks.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
+wait 60
+pwd
+ls -l
+ls -l playbooks/
+chmod 600 playbooks/argela.pem
 # Playbookları sırasıyla çalıştır
-ansible-playbook -i inventory.ini playbook_mirantis.yaml
-ansible-playbook -i inventory.ini playbook_gtp5g.yaml
-ansible-playbook -i inventory.ini playbook_5gc_env.yaml
+ansible-playbook -i playbooks/inventory.ini playbooks/playbook_mirantis.yaml
+ansible-playbook -i playbooks/inventory.ini playbooks/playbook_gtp5g.yaml
+ansible-playbook -i playbooks/inventory.ini playbooks/playbook_5gc_env.yaml
