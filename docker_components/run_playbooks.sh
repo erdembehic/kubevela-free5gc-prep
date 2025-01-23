aa560027aa0b13f6248ba70749f96fdf48fbdc65
Behic <behic.erdem@argela.com>
2025-01-23 17:34:30 +0300

    docker image fixed

--- docker_components/run_playbooks.sh
+++ docker_components/run_playbooks.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-wait 60
-pwd
-ls -l
-ls -l playbooks/
-chmod 600 playbooks/argela.pem
+echo "job has started" ,
+
 # Playbookları sırasıyla çalıştır
 ansible-playbook -i playbooks/inventory.ini playbooks/playbook_mirantis.yaml
 ansible-playbook -i playbooks/inventory.ini playbooks/playbook_gtp5g.yaml
