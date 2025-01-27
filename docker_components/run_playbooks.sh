91464c01612a579566fc7743bf722fef8924ad20
Behic <behic.erdem@argela.com>
2025-01-27 18:38:09 +0300

    initial changes

--- docker_components/run_playbooks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "job has started" ,
-
-# Playbookları sırasıyla çalıştır
-ansible-playbook -i playbooks/inventory.ini playbooks/playbook_mirantis.yaml
-ansible-playbook -i playbooks/inventory.ini playbooks/playbook_gtp5g.yaml
-ansible-playbook -i playbooks/inventory.ini playbooks/playbook_5gc_env.yaml
